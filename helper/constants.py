aa24f7401d28663926f098ad3123474557166c81
Razvan1990 <53650135+Razvan1990@users.noreply.github.com>
2022-10-27 23:14:25 +0300

    Update constants.py

--- helper/constants.py
+++ helper/constants.py
@@ -1,7 +1,10 @@
 OUTPUT_PATH = r"G:\pycharm\pythonProject\BankingApp"
 FILENAME = "bankingDetails.txt"
 OUTPUT_FILENAME = "receipt.txt"
-CARD_INFO=["PIN","NUME", "SUMA DISPONIBILA", "DATA EXPIRARE", "NUMAR CARD"]
+CARD_INFO = ["PIN", "NUME", "SUMA DISPONIBILA", "DATA EXPIRARE", "NUMAR CARD", "NUMAR CONT"]
+AMOUNTS = [10, 20, 50, 100, 200, 500]
+TAXES = [1, 2, 5]
+DICTIONARY_VALUES = {1: 10, 2: 50, 3: 100, 4: 200, 5: 500, 6: 0}
+NUMBER_SPACES_ACCOUNT = 5
 NAME_BANK = '''ATM PUFOSEI HOUSE, str. Piata Republicii, nr 6, bl T850, Gura Humorului, Suceava
 ###########################################################################'''
-
