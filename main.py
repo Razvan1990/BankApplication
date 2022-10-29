c9fdc1fd244696c53645667d027e15c5d34fa102
Razvan1990 <53650135+Razvan1990@users.noreply.github.com>
2022-10-29 13:09:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from computation.compute_elements import Computation
-from operations.bank_reader import BankReader
-from helper.helper_things import Helper
-from operations.bank_operations import BankOperations
 
 
 def run_app():
@@ -9,9 +6,6 @@ def run_app():
     worker.manage_operations()
 
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     run_app()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
